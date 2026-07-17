b3d9d8d4869f3d69cf1de5e10ed79dd06ffddb19
lawal-dev <optimextech@gmail.com>
2026-07-17 13:09:43 +0100

    http round up

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import http from "http";
 const server = http.createServer((req, res) => {
-  res.end("I'm a backend engineer");
+  if (req.url === "/") {
+    res.end("Welcome to homempage");
+  } else if (req.url === "/about") {
+    res.end("welcome to the about page");
+  } else {
+    res.statusCode = 404;
+    res.end("Page not found");
+  }
 });
 
 server.listen(4000, () => {
